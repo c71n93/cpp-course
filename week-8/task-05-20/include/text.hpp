d6af943beeae3c174417e2961572c92459674a98
Roman Korostinskiy <70313618+c71n93@users.noreply.github.com>
2024-11-18 23:15:14 +0300

    fix description

--- week-8/task-05-20/include/text.hpp
+++ week-8/task-05-20/include/text.hpp
@@ -5,12 +5,17 @@
 namespace text {
 
 /**
- * Text concept.
+ * Concept for Text.
  *
  * Attention! The idea for this example was inspired by post
  * yegor256.com/2015/02/26/composable-decorators.html. Implementation of decorator in this post
- * differs from the one given in  wikipedia.org/wiki/Decorator_pattern. The only difference is that
- * this implementation don't has extra Decorator class (which is unnecessary as for me).
+ * differs from the one given by wikipedia.org/wiki/Decorator_pattern. The only difference is that
+ * this implementation doesn't have extra Decorator class (which is unnecessary as for me).
+ *
+ * Also, we don't have inheritance at all in this template implementation. I think it's not
+ * necessary here, because these decorators are used at compile time. And at compile time,
+ * polymorphism is implemented using templates that can be constrained using concepts (Text in our
+ * case).
  */
 template <typename T>
 concept Text = requires(T text) {
